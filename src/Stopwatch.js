6d02297edf23e6699c702f4b503188cc3a8646e2
Cesco <crordaz@gmail.com>
2017-10-24 18:18:33 -0500

    Fran did his hw on time

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count: null
+    };
+    this.going = null;
+    this._contare = this._contare.bind(this);
+    this._fermare = this._fermare.bind(this);
+    this._reIniziare = this._reIniziare.bind(this);
+
+  }
+  _contare(){
+    this.setState({
+      count: this.state.count+1 ||0
+    })
+  }
+
+  _fermare(){
+
+  }
+  _reIniziare(){
+    this.setState({
+      count: 0
+    })
+
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.count}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this._reIniziare}>Reset</button>
+          <button onClick={(event)=>{this.going = setInterval(this._contare, 1000)}}>Start</button>
+          <button onClick={(event)=>{clearInterval(this.going)}}>Pause</button>
         </div>
       </div>
     );
