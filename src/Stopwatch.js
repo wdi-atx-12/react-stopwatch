10e97d2aa13528f1e8bb50552f70408105a4b2c1
Cesco <crordaz@gmail.com>
2017-10-24 18:20:53 -0500

    Fran did his hw on time pt.2

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -18,10 +18,6 @@ class Stopwatch extends Component {
       count: this.state.count+1 ||0
     })
   }
-
-  _fermare(){
-
-  }
   _reIniziare(){
     this.setState({
       count: 0
@@ -34,7 +30,7 @@ class Stopwatch extends Component {
         <h1>{this.state.count}</h1>
         <div className="controls">
           <button onClick={this._reIniziare}>Reset</button>
-          <button onClick={(event)=>{this.going = setInterval(this._contare, 1000)}}>Start</button>
+          <button onClick={(event)=>{this.going = setInterval(this._contare, 100)}}>Start</button>
           <button onClick={(event)=>{clearInterval(this.going)}}>Pause</button>
         </div>
       </div>
