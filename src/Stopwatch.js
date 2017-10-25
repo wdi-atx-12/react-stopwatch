3ffcd80c7507943f833bde1bf1f0b9cbf7a7f049
Andrew Chavez <chavandrew@gmail.com>
2017-10-25 08:55:38 -0500

    stopwatch hw

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      timer: 0,
+    }
+    this.reset = this.reset.bind(this);
+    this.start = this.start.bind(this);
+    this.pause = this.pause.bind(this);
+    this.counter = () => {
+      this.setState({
+        timer: this.state.timer + 1
+      })
+    }
+  }
+
+
+  startTimer(e){
+    e.preventDefault();
+  }
+  stopTimer(e){
+    e.preventDefault();
+  }
+  resetTimer(e){
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.timer}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.reset}>Reset</button>
+          <button onClick={this.start}>Start</button>
+          <button onClick={this.pause}>Pause</button>
         </div>
       </div>
     );
