347d47736423c858c4ddee05943559ebdee03d4e
Karla Moreno <moreno_karla@live.com>
2017-10-25 07:06:24 -0500

    created simple, 1 second stopwatch

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,50 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      time: 0,
+      interval: 0
+    }
+    this._resetTime = this._resetTime.bind(this);
+    this._startTime = this._startTime.bind(this);
+    this._pauseTime = this._pauseTime.bind(this);
+  }
+
+  _resetTime(e){
+    this.setState ({
+      time: 0
+    })
+  }
+
+  _startTime(e){
+    console.log("start time button has been clicked");
+    this.changeTime = () => {
+      this.setState({
+        time: this.state.time + 1
+      })
+    }
+    this.setState ({
+        interval: setInterval(this.changeTime, 1000)
+    })
+  }
+
+  _pauseTime(e){
+    console.log("pause time button has been clicked");
+    clearInterval(this.state.interval)
+  }
+
   render() {
+    var time = this.state.time;
+
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{time}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this._resetTime}>Reset</button>
+          <button onClick={this._startTime}>Start</button>
+          <button onClick={this._pauseTime}>Pause</button>
         </div>
       </div>
     );
