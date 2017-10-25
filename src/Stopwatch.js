c1e0f0eb1a982a8edd7b314d023b7fe05c30cc28
Peter Kong <bansuki@gmail.com>
2017-10-24 22:07:44 -0500

    made method stubs and added to jsx

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,40 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.autoBind();
+  }
+
+  autoBind() {
+    for(let m in this) {
+      if(typeof(this[m])==='function' && m.match(/_\w+/)) {
+        this[m] = this[m].bind(this)
+      }
+    }
+  }
+
+  _onReset(e) {
+
+  }
+
+  _onStart(e) {
+
+  }
+
+  _onPause(e) {
+
+  }
+
   render() {
     return (
       <div className="stopwatch">
         <h1>0</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onReset={this._onReset}>Reset</button>
+          <button onStart={this._onStart}>Start</button>
+          <button onPause={this._onPause}>Pause</button>
         </div>
       </div>
     );
