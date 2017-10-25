df9026ef87b6fc13ebf8aeb1f212fc8ca189484c
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-10-24 22:09:01 -0500

    Jesse Lewis - stuck on binding it seems, everything I am trying to do is returning undefined

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,59 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(props){
+    super(props);
+      this.stopTime = this.stopTime.bind(this);
+      this.startTimer = this.startTimer.bind(this);
+      this.pauseTimer = this.pauseTimer.bind(this);
+      this.resetTimer = this.resetTimer.bind(this);
+
+      this.state = {
+        currentTime: 0,
+        stopTime: null
+      };
+
+  }
+  updateWatch(time){
+    var display = document.getElementById('#watch');
+    display.innerHTML = 'time: '+time;
+  }
+  startTimer(x){
+    if(!stopTime){
+      var currentTime = x;
+      currentTime++;
+      this.setState({ currentTime: currentTime});
+
+      //call updateWatch to update the watch display
+      updateWatch(x);
+      //wait a second then call me again until my state of stopTime is true
+      setTimeout(startTimer(x), 1000);
+    } else {
+      console.log('stopTime is true');
+    }
+  }
+  pauseTimer(){
+    if(!stopTime){
+       stopTime = true
+     }else if(stopTime){
+       stopTime = false
+     } else {
+      console.log('look, I am just a broken pause button.');
+     }
+
+  }
+  resetTimer(){
+    currentTime = 0;
+    updateWatch(currentTime);
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1 id="watch">0</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.resetTimer}>Reset</button>
+          <button onClick={this.startTimer}>Start</button>
+          <button onclick={this.pauseTimer}>Pause</button>
         </div>
       </div>
     );
